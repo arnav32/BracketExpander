3d01f5b4981c85f921917b5a89cfe4b9ffc99396
arnav32 <arnav@arnavprasad.com>
2024-05-20 18:09:41 +0100

    Removed temporary test code for cropping image (WIP)

--- main.py
+++ main.py
@@ -157,94 +157,22 @@ fig = latex2image(latex_expression, image_name, image_size_in=(16, 9), fontsize=
 
 img = Image.open("expanded_result.png")
 
-
-
-from PIL import Image, ImageChops
-
-def trim(im):
-    bg = Image.new(im.mode, im.size, im.getpixel((0,0)))
-    diff = ImageChops.difference(im, bg)
-    diff = ImageChops.add(diff, diff, 2.0, -100)
-    bbox = diff.getbbox()
-    if bbox:
-        return im.crop(bbox)
-
-imgg = trim(fig)
-
 img.show()
-imgg.show()
 
+# test data: (-0.5x^0.8 - 4)(0.1x - 5.2x^2.2)(-9.8x^2 - 12 + 2x)
+
+
+# # Trying to crop image to just equation  (using: https://stackoverflow.com/questions/10615901/trim-whitespace-using-pil) (may be helpful also: https://stackoverflow.com/questions/28759253/how-to-crop-the-internal-area-of-a-contour)
+# from PIL import Image, ImageChops
 #
+# def trim(im):
+#     bg = Image.new(im.mode, im.size, im.getpixel((0,0)))
+#     diff = ImageChops.difference(im, bg)
+#     diff = ImageChops.add(diff, diff, 2.0, -100)
+#     bbox = diff.getbbox()
+#     if bbox:
+#         return im.crop(bbox)
 #
-# def preproces_image(
-#     image,
-#     *,
-#     kernel_size=15,
-#     crop_side=50,
-#     blocksize=35,
-#     constant=15,
-#     max_value=255,
-# ):
-#     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-#     bit = cv2.bitwise_not(gray)
-#     image_adapted = cv2.adaptiveThreshold(
-#         src=bit,
-#         maxValue=max_value,
-#         adaptiveMethod=cv2.ADAPTIVE_THRESH_MEAN_C,
-#         thresholdType=cv2.THRESH_BINARY,
-#         blockSize=blocksize,
-#         C=constant,
-#     )
-#     kernel = np.ones((kernel_size, kernel_size), np.uint8)
-#     erosion = cv2.erode(image_adapted, kernel, iterations=2)
-#     return erosion[crop_side:-crop_side, crop_side:-crop_side]
-#
-# def find_edges(image_preprocessed, *, bw_threshold=150, limits=(0.2, 0.15)):
-#     mask = image_preprocessed < bw_threshold
-#     edges = []
-#     for axis in (1, 0):
-#         count = mask.sum(axis=axis)
-#         limit = limits[axis] * image_preprocessed.shape[axis]
-#         index_ = np.where(count >= limit)
-#         _min, _max = index_[0][0], index_[0][-1]
-#         edges.append((_min, _max))
-#     return edges
-#
-#
-# def adapt_edges(edges, *, height, width):
-#     (x_min, x_max), (y_min, y_max) = edges
-#     x_min2 = x_min
-#     x_max2 = x_max + min(250, (height - x_max) * 10 // 11)
-#     # could do with less magic numbers
-#     y_min2 = max(0, y_min)
-#     y_max2 = y_max + min(250, (width - y_max) * 10 // 11)
-#     return (x_min2, x_max2), (y_min2, y_max2)
-#
-#
-# filename_in = "NHnV7.png"
-# filename_out = "res_NHnV7.png"
-#
-# image = cv2.imread(str(filename_in))
-# height, width = image.shape[0:2]
-# image_preprocessed = preproces_image(image)
-# edges = find_edges(image_preprocessed)
-# (x_min, x_max), (y_min, y_max) = adapt_edges(
-#     edges, height=height, width=width
-# )
-# image_cropped = image[x_min:x_max, y_min:y_max]
-# cv2.imwrite(str(filename_out), image_cropped)
-#
+# img_trim = trim(fig)
+# img_trim.show()
 
-
-
-
-
-
-
-
-
-
-
-# test: (-0.5x^0.8 - 4)(0.1x - 5.2x^2.2)(-9.8x^2 - 12 + 2x)
-# C:\Users\spsah\PycharmProjects\bracket_expander\venv\Scripts\activate.bat
-# C:\Users\spsah\PycharmProjects\bracket_expander\venv\Scripts\python.exe D:/Program Files/JetBrains/PyCharm Community Edition 2022.3.1/plugins/python-ce/helpers/packaging_tool.py install cv2
